01806654d31761fd8aeea75a7b35d5490691e15e
Pavan Tamrakar <72463719+PavanTamrakar@users.noreply.github.com>
2022-08-31 14:23:26 +0530

    Update GandhiDAO.sol

--- GandhiDAO.sol
+++ GandhiDAO.sol
@@ -1,1 +1,19 @@
+//SPDX-Licence-Identifier: MIT
 
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IGandhiDAO{
+    function getPrice() external view returns (uint256);
+
+    function available(uint256 _tokenId) external view returns (bool);
+
+    function purchase(uint256 _tokenId) external payable;
+}
+
+interface IGandhiMoneyNFT {
+    function balanceOf(address owner) external view returns (uint256);
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
+}
